96c4ea2b6adf27e78e60c82969509b04bac2282e
NACV <na.cifva7@gmail.comm>
2026-09-19 21:54:28 -0300

    Creacion entorno principal spring: auditoria

---
+++ ms-rutaexpress-audit/src/test/java/com/na/rutaexpress/audit/MsRutaexpressAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.na.rutaexpress.audit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRutaexpressAuditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
